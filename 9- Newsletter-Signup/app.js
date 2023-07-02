97c9e9c29704aa52444476bac3aeaff62d9c54f4
Bruno Barreto <brunobarreto@Brunos-MacBook-Pro.local>
2023-07-02 08:25:28 +0100

    newsletter signup

---
+++ 9- Newsletter-Signup/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(req, res){
+
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
