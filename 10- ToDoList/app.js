5066443f6848b04ddf067ab3a06647d478c0b3ee
Bruno Barreto <brunobarreto@Brunos-MacBook-Pro.local>
2023-07-06 07:45:53 +0100

    improving ToDoList

--- 10- ToDoList/app.js
+++ 10- ToDoList/app.js
@@ -9,29 +9,30 @@ app.get('/', (req, res) => {
     var today = new Date();
     var currentDay = today.getDay();
     var day = "";
-    if (currentDay === 6) {
-        day = "Monday";
-    }
-    else if (currentDay === 0) {
-        day = "Sunday";
-    }
-    else if (currentDay === 1) {
-        day = "Monday";
-    }
-    else if (currentDay === 2) {
-        day = "Tuesday";
-    }
-    else if (currentDay === 3) {
-        day = "Wednesday";
-    }
-    else if (currentDay === 4) {
-        day = "Thursday";
-    }
-    else if (currentDay === 5) {
-        day = "Friday";
-    }
-    else {
-        day = "Saturday";
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+        console.log("Error: current day is equal to: " + currentDay);
     }
 
     var week = "";
